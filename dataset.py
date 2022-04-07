c0a317a557a22f0c55efbe217641ed30bb1982dd
Anish Moorthy <anish@pop-os.localdomain>
2022-04-06 19:17:57 -0700

    Read from gz or regular

--- dataset.py
+++ dataset.py
@@ -14,25 +14,37 @@ parser.add_argument('out_file', help='Idk you export the data here or something'
 
 args = parser.parse_args()
 
-if __name__ == "__main__":
-    with gzip.open(args.rawdata_file) as f:
-
-        # We just totally ignore the userhashes for now, they are too big
-        line_pattern = re.compile('(.*) UTC,.*==,#(.*),"(.*)"')
-
-        # Garbage formal line
+def gz_lines(fname):
+    with gzip.open(fname) as f:
         f.readline()
+        for line in f:
+            yield line.decode('utf-8')
 
-        n_lines = 0
-        # Parsing the actual data
-        for line in tqdm(f):
-            line = line.decode('utf-8')
-            match = line_pattern.match(line).groups()
-            tstr, color, coords = match
-            if "." not in tstr:
-                tstr = tstr + ".000"
-            tobj = datetime.strptime(str(tstr + "000"), "%Y-%m-%d %H:%M:%S.%f")
-            unix_ts = int(tobj.timestamp()*1000)
-            n_lines += 1
+def file_lines(fname):
+    with open(fname) as f:
+        f.readline()
+        for line in f:
+            yield line
 
-        print(n_lines)
+if __name__ == "__main__":
+
+    lines_fn = None
+    if args.rawdata_file[-3:] == ".gz":
+        lines_iter = gz_lines(args.rawdata_file)
+    else:
+        lines_iter = file_lines(args.rawdata_file)
+
+    # Ignore user hashes, they are very big
+    line_pattern = re.compile('(.*) UTC,.*==,#(.*),"(.*)"')
+    n_lines = 0
+
+    for line in tqdm(lines_iter):
+        match = line_pattern.match(line).groups()
+        tstr, color, coords = match
+        if "." not in tstr:
+            tstr = tstr + ".000"
+        tobj = datetime.strptime(str(tstr + "000"), "%Y-%m-%d %H:%M:%S.%f")
+        unix_ts = int(tobj.timestamp()*1000)
+        n_lines += 1
+
+    print(n_lines)
