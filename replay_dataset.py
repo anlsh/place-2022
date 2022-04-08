174b78a45fd5e80dc2521cca0392898435d97f6b
Anish Moorthy <anish@pop-os.localdomain>
2022-04-08 16:20:51 -0700

    Commit a bit

--- replay_dataset.py
+++ replay_dataset.py
@@ -2,67 +2,100 @@ from PIL import Image
 import math
 import argparse
 import numpy as np
+import os
+from pathlib import Path
+from collections import deque
 
 from tqdm import tqdm
 
 SIDE_LENGTH = 2000
 
-PALETTE = [
-    0x000000,
-    0x00756F,
-    0x009EAA,
-    0x00A368,
-    0x00CC78,
-    0x00CCC0,
-    0x2450A4,
-    0x3690EA,
-    0x493AC1,
-    0x515252,
-    0x51E9F4,
-    0x6A5CFF,
-    0x6D001A,
-    0x6D482F,
-    0x7EED56,
-    0x811E9F,
-    0x898D90,
-    0x94B3FF,
-    0x9C6926,
-    0xB44AC0,
-    0xBE0039,
-    0xD4D7D9,
-    0xDE107F,
-    0xE4ABFF,
-    0xFF3881,
-    0xFF4500,
-    0xFF99AA,
-    0xFFA800,
-    0xFFB470,
-    0xFFD635,
-    0xFFF8B8,
-    0xFFFFFF
-]
+PALETTE = np.array([
+    [0x00, 0x00, 0x00],
+    [0x00, 0x75, 0x6F],
+    [0x00, 0x9E, 0xAA],
+    [0x00, 0xA3, 0x68],
+    [0x00, 0xCC, 0x78],
+    [0x00, 0xCC, 0xC0],
+    [0x24, 0x50, 0xA4],
+    [0x36, 0x90, 0xEA],
+    [0x49, 0x3A, 0xC1],
+    [0x51, 0x52, 0x52],
+    [0x51, 0xE9, 0xF4],
+    [0x6A, 0x5C, 0xFF],
+    [0x6D, 0x00, 0x1A],
+    [0x6D, 0x48, 0x2F],
+    [0x7E, 0xED, 0x56],
+    [0x81, 0x1E, 0x9F],
+    [0x89, 0x8D, 0x90],
+    [0x94, 0xB3, 0xFF],
+    [0x9C, 0x69, 0x26],
+    [0xB4, 0x4A, 0xC0],
+    [0xBE, 0x00, 0x39],
+    [0xD4, 0xD7, 0xD9],
+    [0xDE, 0x10, 0x7F],
+    [0xE4, 0xAB, 0xFF],
+    [0xFF, 0x38, 0x81],
+    [0xFF, 0x45, 0x00],
+    [0xFF, 0x99, 0xAA],
+    [0xFF, 0xA8, 0x00],
+    [0xFF, 0xB4, 0x70],
+    [0xFF, 0xD6, 0x35],
+    [0xFF, 0xF8, 0xB8],
+    [0xFF, 0xFF, 0xFF]
+], dtype=np.uint8)
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('csv', help='Path to the cleaned csv')
-parser.add_argument('outfile', help='Png to export the final png to')
-parser.add_argument('--target_seqno', help='Terminate at last frame before @ this seqno', required=False, default=math.inf, type=int)
+# parser.add_argument('outfile', help='Png to export the final png to')
+# parser.add_argument('--target_seqno', help='Terminate at last frame before @ this seqno', required=False, default=math.inf, type=int)
 parser.add_argument('--target_millis', help='Terminate at last frame before this millis', required=False, default=math.inf, type=int)
+
+# Args concerning dumps to png files... christ I'm gonna kill my storage
+parser.add_argument('--dump_millis', required=False, default=None, type=int)
+parser.add_argument('--dumpdir', required=False, default=None, type=str)
+parser.add_argument('--dumplast', required=False, default=True, type=bool)
+parser.add_argument('--dumpops', required=False, default=None, type=bool)
 
 args = parser.parse_args()
 
 if __name__ == "__main__":
 
-    with open(args.csv) as f:
-        f.readline()
-        seqno = 1
+    # Have to do some argument validation...
+    if bool(args.dump_millis) != bool (args.dumpdir):
+        raise RuntimeError("DumpDir and dumpmillis must both be specified")
+    if args.dump_millis == 0:
+        raise RuntimeError("Dumping every 0 millis... Not even defined!")
+    elif args.dumpops and not args.dumpdir:
+        raise RuntimeError("NoDumplast and/or dumpops should only be set when dumping!")
 
-        arr = np.full((SIDE_LENGTH, SIDE_LENGTH), 0xFFFFFF, dtype=np.int32)
+    if args.dumpdir:
+        os.makedirs(args.dumpdir, exist_ok=False)
+
+    with open(args.csv) as f:
+        # Garbage line describing the "format" which is for losers
+        f.readline()
+        dump_i = 0
+
+        arr = np.full((SIDE_LENGTH, SIDE_LENGTH, 3), 0xFF, dtype=np.uint8)
+        curr_ops = deque()
+
+        dumpdir_path = Path(args.dumpdir) if args.dumpdir else None
+        def dump_for_timestamp(arr, timestamp, curr_ops):
+            prefix = "{:012d}".format(timestamp)
+            im = Image.fromarray(arr)
+            im.save(dumpdir_path / f"{prefix}.png")
+            if args.dumpops:
+                with open(dumpdir_path / f"{prefix}.ops", "w+") as f:
+                    for line in curr_ops:
+                        f.write(f"{line}\n")
+                curr_ops.clear()
+
+
+        toff = palette_i = censor = x0 = y0 = x1 = y1 = seqno = None
 
         for line in tqdm(f):
-            if seqno >= args.target_seqno:
-                break
-            seqno += 1
             parts = line.split(",")
 
             toff = int(parts[0])
@@ -73,9 +106,21 @@ if __name__ == "__main__":
             x1 = int(parts[5])
             y1 = int(parts[6])
             # uint_id = int(parts[7])
-            # seqno = int(parts[8])
+            seqno = int(parts[8])
 
+            if toff >= args.target_millis:
+                break
+            if args.dumpdir and (toff / args.dump_millis > dump_i):
+                dump_for_timestamp(
+                    arr,
+                    toff // args.dump_millis * args.dump_millis,
+                    curr_ops
+                )
+                dump_i = math.ceil(toff / args.dump_millis)
+
+            if args.dumpops:
+                curr_ops.append(line)
             arr[y0, x0] = PALETTE[palette_i - 1]
 
-        im = Image.fromarray(arr)
-        im.save(args.outfile)
+        if args.dumplast:
+            dump_for_timestamp(arr, toff + 1, curr_ops)
