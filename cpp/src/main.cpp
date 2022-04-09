e67333951bb05d71ac1c8ec78f56f9b84891595d
Anish Moorthy <anish@pop-os.localdomain>
2022-04-09 13:45:37 -0700

    Oh fuck, NOW it compiles. I'm using rust

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include "tqdm/tqdm.h"
 #include "png.h"
+#include "tqdm/tqdm.h"
 
 int main() {
     std::cout << "Foobar" << std::endl;
-    for (int i : tqdm::range(30000000));
+    // for (int i : tqdm::range(30000000));
 }
