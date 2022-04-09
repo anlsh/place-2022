36fb02902ff6ca964f572ad26c6c9b6b7684ad1b
Anish Moorthy <anish@pop-os.localdomain>
2022-04-08 17:54:52 -0700

    Make some changes for binary, no idea if works

--- replay_dataset.py
+++ replay_dataset.py
@@ -57,23 +57,91 @@ parser.add_argument('--dump_millis', required=False, default=None, type=int)
 parser.add_argument('--dumpdir', required=False, default=None, type=str)
 parser.add_argument('--dumplast', required=False, default=True, type=bool)
 parser.add_argument('--dumpops', required=False, default=None, type=bool)
+parser.add_argument('--ensure_encoding', required=False, default=False, type=bool)
 
 args = parser.parse_args()
 
 class PlaceOp:
-    def __init__(self, csvline):
+    def __init__(self, toff, censor, c0, r0, c1, r1, palette_i, old_palette_i):
+        self.toff = toff
+        self.censor = censor
+        self.c0 = c0
+        self.r0 = r0
+        self.c1 = c1
+        self.r1 = r1
+        self.palette_i = palette_i
+        self.old_palette_i = old_palette_i
+
+    @classmethod
+    def from_csv_and_pic(cls, csvline, palette_arr):
         parts = line.split(",")
 
-        self.toff = int(parts[0])
-        self.palette_i = int(parts[1])
-        self.censor = (parts[2] == "t")
-        self.x0 = int(parts[3])
-        self.y0 = int(parts[4])
-        self.x1 = int(parts[5])
-        self.y1 = int(parts[6])
-        # Unused for now because oh my god
-        # self.uint_id = int(parts[7])
-        self.seqno = int(parts[8])
+        toff = int(parts[0])
+        censor = (parts[2] == "t")
+        c0 = int(parts[3])
+        r0 = int(parts[4])
+        c1 = int(parts[5])
+        r1 = int(parts[6])
+
+        palette_i = int(parts[1])
+        old_palette_i = palette_arr[self.c0, self.r0]
+
+        self.uint_id = int(parts[7])
+
+        return cls(toff, censor, c0, r0, c1, r1, palette_i, old_palette_i)
+
+    def to_binary(self,):
+        # We fit each operation into 16 bytes
+        encoding = bytearray(16)
+        # Some things to know for the encoding
+        # The max toff (last pixel placed) was 300589892 millis from t=0
+        # This required 29 bits, so fit into 4 bytes
+        encoding[:4] = self.toff.to_bytes(4, byteorder = 'big')
+
+        # First bit of the encoding encodes whether this operation was a use of
+        # the rectangle tool
+        encoding[0] |= 8 if self.censor else 0
+
+        # The max coordinate for any of the rows/cols is 2000, which takes 11
+        # bits
+        # We use 2 bytes for each of c0, r0
+        # c1 and r1 will be very infrequently used, so we stuff both into three
+        # ybytes
+        # TODO Could save 2 bytes relatively easily here
+        encoding[4:6] = self.c0.to_bytes(2, byteorder = 'big')
+        encoding[6:8] = self.r0.to_bytes(2, byteorder = 'big')
+
+        c1_bytes = self.c1.to_bytes(2, byteorder = 'big')
+        r1_bytes = self.r1.to_bytes(2, byteorder = 'big')
+
+        encoding[8] = (c1_bytes[0] << 2) + (c1_bytes[1] >> 2)
+        encoding[9] = ((c1_bytes[1] & 3) << 2) + r1_bytes[0]
+        encoding[10] = r1_bytes[1]
+
+        # Each palette_i < 32 ie takes 5 bits, we just give each a byte
+        # We store both new and old palette_i to make bidirectional playback easier
+        encoding[11] = self.palette_i.to_bytes(1, byteorder = 'big')
+        encoding[12] = self.old_palette_i.to_bytes(1, byteorder = 'big')
+
+        # max uint_id in dataset is 10381163, requiring 24 bits: so another 3
+        # bytes
+        encoding[12:16] = self.uint_id.to_bytes(3, byteorder = 'big')
+
+    @classmethod
+    def from_binary(cls, bytearr):
+        # TODO this is WRONG oh my GOD I'm not accounting for the censor bit!!
+        toff = int.from_bytes(bytearr[:4] & 0x8000, byteorder = 'big')
+        censor = bool(bytearr[0] & 3)
+        c0 = int.from_bytes(bytearr[4:6], byteorder = 'big')
+        r0 = int.from_bytes(bytearr[6:8], byteorder = 'big')
+        c1 = (encoding[8] << 2) + (encoding[9] >> 2)
+        r1 = ((encoding[9] & 0xF) << 4) + encoding[10]
+        palette_i = int(encoding[11])
+        old_palette_i = int(encoding[12])
+        uint_id = int.from_bytes(bytearr[13:], byteorder = 'big')
+
+        return cls(toff, censor, c0, r0, c1, r1, palette_i, old_palette_i)
+
 
 if __name__ == "__main__":
 
@@ -94,36 +162,50 @@ if __name__ == "__main__":
         dump_i = 0
 
         arr = np.full((SIDE_LENGTH, SIDE_LENGTH, 3), 0xFF, dtype=np.uint8)
+        palette_arr = np.full((SIDE_LENGTH, SIDE_LENGTH), 31, dtype=np.uint8)
         curr_ops = deque()
 
         dumpdir_path = Path(args.dumpdir) if args.dumpdir else None
-        def dump_for_timestamp(arr, timestamp, curr_ops):
+        def dump_for_timestamp(arr, timestamp, seqno, curr_ops):
             prefix = "{:012d}".format(timestamp)
             im = Image.fromarray(arr)
             im.save(dumpdir_path / f"{prefix}.png")
             if args.dumpops:
                 with open(dumpdir_path / f"{prefix}.ops", "rb") as f:
+                    # Write the number of ops in the file
+                    f.write(len(curr_ops).to_bytes(4, byteorder='big'))
+                    # Write the sequence number of the first op in the file
+                    f.write(seqno.to_bytes(4, bytesorder='big'))
                     for op in curr_ops:
-                        f.write(op.bin())
+                        f.write(op.to_binary())
                 curr_ops.clear()
 
         op = None
+        seqno = 0
         for line in tqdm(f):
-            op = PlaceOp(line)
+            seqno += 1
+            op = PlaceOp.from_csv_and_palettepic(line, palette_arr)
+            encoded_op = op.to_binary()
+
+            if args.ensure_encoding:
+                decoded_op = PlaceOp.from_binary(encoded_op)
+                if op != decoded_op:
+                    raise RuntimeError(f"Coding error:\nOp {op}\nEncoded as {encoded_op}\ndecoded as {decoded_op}")
 
             if op.toff >= args.target_millis:
                 break
             if args.dumpdir and (op.toff / args.dump_millis > dump_i):
                 dump_for_timestamp(
                     arr,
-                    op.toff // args.dump_millis * args.dump_millis,
+                    op.toff // args.dump_millis * args.dump_millis, seqno,
                     curr_ops
                 )
                 dump_i = math.ceil(op.toff / args.dump_millis)
 
             if args.dumpops:
-                curr_ops.append(op)
-            arr[op.y0, op.x0] = PALETTE[op.palette_i - 1]
+                curr_ops.append(encoded_op)
+            palette_arr[op.c0, op.r0] = op.palette_i - 1
+            arr[op.c0, op.r0] = PALETTE[op.palette_i - 1]
 
         if args.dumplast:
-            dump_for_timestamp(arr, op.toff + 1, curr_ops)
+            dump_for_timestamp(arr, op.toff + 1, seqno, curr_ops)
